a2c370f07171b3ba7c9ed87430b67a9263429271
Gabriel Brogni <92529704+gbrogni@users.noreply.github.com>
2026-08-26 08:04:33 -0300

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const map = new Map<string, string[]>();
+
+        for (const word of strs) {
+            const count = new Array(26).fill(0);
+
+            for (const char of word) {
+                count[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+            }
+
+            const key = count.join(",");
+
+            if (!map.has(key)) {
+                map.set(key, []);
+            }
+
+            map.get(key).push(word);
+        }
+
+        return Array.from(map.values());
+    }
+}
